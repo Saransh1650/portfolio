c1bb6e17c6925ea38c9349a9e6cc00f663c0817f
Saransh Singhal <singhalsaransh40@gmail.com>
2026-06-24 13:43:23 +0530

    fix: posthog

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/array/:path*",
-        destination: "https://us-assets.i.posthog.com/array/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-    ];
-  },
-  skipTrailingSlashRedirect: true,
+  output: "export",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
